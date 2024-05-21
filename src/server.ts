28f98d76afa97e47b0bd1766e2da9d7221c451bb
imran0dot <imran0dot@gmail.com>
2024-05-21 12:39:59 +0600

    db connect

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,20 @@
 import app from "./app";
 import config from '../src/config/index';
+import mongoose from "mongoose";
+
+const main = async() => {
+    try{
+        const db_connect = await mongoose.connect(config.db_url as string);
+        if(db_connect){
+            console.log(`db connect successfully`)
+        }
+    }
+    catch(err){
+        throw new Error(`DB Connection error ${err}`)
+    }
+};
+
+main();
 
 app.listen(config.port, () => {
     console.log(`your server is running on the port http://localhost:${config.port}`)
