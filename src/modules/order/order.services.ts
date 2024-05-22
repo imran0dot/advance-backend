6fbd5470de9639fcff63006329eb29fd192fc147
imran0dot <imran0dot@gmail.com>
2024-05-22 13:08:36 +0600

    update in stock if the quantity is zero

--- src/modules/order/order.services.ts
+++ src/modules/order/order.services.ts
@@ -2,6 +2,7 @@ import ProductModel from "../product/product.model";
 import { Order } from "./order.interface"
 import OrderModel from "./order.model";
 
+// create order into database 
 const createOrderIntoDB = async (orderData: Order) => {
     // find product to DB 
     const product = await ProductModel.findOne({ _id: orderData.productId });
@@ -14,9 +15,9 @@ const createOrderIntoDB = async (orderData: Order) => {
         // Update the quantity of the product
         await ProductModel.updateOne(
             { _id: orderData.productId },
-            { $inc: { 'inventory.quantity': -1 } }
+            { $inc: { 'inventory.quantity': -orderData.quantity } }
         );
-        
+
         return await OrderModel.create(orderData);
     }
     else {
@@ -24,12 +25,14 @@ const createOrderIntoDB = async (orderData: Order) => {
     }
 };
 
+// get all order from database 
 const getAllOrderFromDB = async (email: string | null) => {
     if (email) {
         return await OrderModel.find({ email: email });
     }
     return await OrderModel.find()
 }
+
 export const orderServices = {
     createOrderIntoDB,
     getAllOrderFromDB
